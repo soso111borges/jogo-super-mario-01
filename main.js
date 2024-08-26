2e03d095f683faab0aca9aed42004e6c988d0639
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-08-26 19:08:43 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+const mario = document.querySelector (".mario");
+const pipe = document.querySelector("pipe");
 
+ const jump = () =>  {
+    mario.classList.add("jump");
+
+    setTimeout(() => {
+        mario.classList.remove("jump")
+    }
+    ,500);
+}
+
+       const loop = setInterval(() => {}(;
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    document.addEventListener("keydown",jump);
