dc7add3e1cb727ab8af8d8fe95b7371455c80f7e
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-09-09 19:21:11 -0300

    .

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-const mario = document.querySelector (".mario");
-const pipe = document.querySelector("pipe");
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
- const jump = () =>  {
+const jump = () => {
     mario.classList.add("jump");
 
     setTimeout(() => {
         mario.classList.remove("jump")
-    }
+    } 
     ,500);
 }
 
-       const loop = setInterval(() => {}(;
-       const pipePosition = pipe.offsetLeft;
-       const marioPosition = +windw.getComputedStyle(mario).bottom.replace("px","");
+    const loop = setInterval (() => {
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-  if(pipePosition => 120 && pipePosition > 0 && marioPosition < 80) {
-   pipe.style.animation = "none";
-   pipe.style.left = `${pipePosition}px`;
+        if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+            pipe.style.animation = "none";
+            pipe.style.left = `${pipePosition}px`;
 
-   mario.style.animation = "none";
-   mario.style.left = `${marioPosition}px`;
+            mario.style.animation = "none";
+            mario.style.left = `${marioPosition}px`;
 
-mario.src = "./img/game-over.png";
-mario.computedStyleMap.width = "75px";
-mario.style.marginLeft = "50px";
-  
- clearInterval(loop);
-  }
-  },10);
+            mario.src = "./img/game-over.png";
+            mario.style.width = "75px";
+            mario.style.marginLeft = "50px";
 
-  document.addEventListener("keydown",jump);
+            clearInvertaval(loop);
+        }
+    },10);
+        
+document.addEventListener("keydown",jump);
