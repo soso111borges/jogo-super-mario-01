ad1ca768f2ab935edc48fc912276d0ec9f2950e1
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-09-18 11:17:23 -0300

    .

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const pipe = document.querySelector(".pipe");
 const nuvem = document.querySelector(".nuvem")
 const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
+const scoreElement = document.querySelector("score");
 
 audioStart = new Audio("./sound/audio_theme.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false; 
+let score = 0;
 
 const startGame = () => {
 gameStarted = true;
@@ -32,6 +34,13 @@ const jump = () => {
     ,500);
  }
  }
+
+  const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
+  }
+
+
     const loop = setInterval (() => {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
@@ -53,8 +62,10 @@ const jump = () => {
 
             clearInterval(loop);
             gameOverScreen.style.display = "flex";
-
-         } 
+        } else if (pipePosition < 0 && gemeStarted) {
+          updateScore();
+          pipe.style.left = '';
+        }
     },10);
         
 
